4bac1fde7de91dcbc46ca9a6b4fe14fbd5d2bc79
kennethbollen <31512761+kennethbollen@users.noreply.github.com>
2018-07-23 17:21:28 +0100

    defending

--- get_stats.py
+++ get_stats.py
@@ -20,3 +20,12 @@ driver.get('https://members.fantasyfootballscout.co.uk/')
 driver.find_element(by='name', value='username').send_keys(username)
 driver.find_element(by='name', value='password').send_keys(password)
 driver.find_element(by='name', value='login').click()
+
+#get defending data
+driver.get('https://members.fantasyfootballscout.co.uk/team-stats/defending/')
+body = table.find_element_by_tag_name('tbody')
+body_rows = body.find_elements_by_tag_name('tr')
+for row in body_rows:
+	data = row.find_elements_by_tag_name('td')
+	for datum in data:
+		print(datum.text)
