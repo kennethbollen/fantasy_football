acc53a3d3fd70123d4c225013229862868401508
kennethbollen <31512761+kennethbollen@users.noreply.github.com>
2018-07-25 14:18:56 +0100

    Loop for file scrapping
    consolidated all file scrapping codes into one loop

--- get_stats.py
+++ get_stats.py
@@ -16,7 +16,9 @@ def_data = []
 att_data = []
 set_pieces_data = []
 ex_data = []
-
+metrics = [involv_data, dist_data, def_data, att_data, set_pieces_data, ex_data]
+web_metrics = ['involvement', 'distribution', 'defending', 'goal-threat', 'set-pieces', 'expected']
+	       
 #using Google Chrome browser, requires Chrome Driver to be installed into PATH
 driver = webdriver.Chrome('C:/Chrome_Driver/chromedriver.exe')
 
@@ -28,117 +30,18 @@ driver.find_element(by='name', value='username').send_keys(username)
 driver.find_element(by='name', value='password').send_keys(password)
 driver.find_element(by='name', value='login').click()
 
-#get involvement data
-driver.get('https://members.fantasyfootballscout.co.uk/team-stats/involvement/')
-table = driver.find_element_by_class_name('stats')
-body = table.find_element_by_tag_name('tbody')
-body_rows = body.find_elements_by_tag_name('tr')
-
-for row in body_rows:
-    data = row.find_elements_by_tag_name('td')
-    file_row = []
-    for datum in data:
-        datum_text = datum.text
-        file_row.append(datum_text)
-    involv_data.append(','.join(file_row))
-
-#write involvement data into csv
-with open(save_location + '/involvement_data.csv', 'wb') as f:
-	wr = csv.writer(f, dialect='excel')
-	wr.writerows(involv_data)
-
-#get distribution data
-driver.get('https://members.fantasyfootballscout.co.uk/team-stats/distribution/')
-table = driver.find_element_by_class_name('stats')
-body = table.find_element_by_tag_name('tbody')
-body_rows = body.find_elements_by_tag_name('tr')
-
-for row in body_rows:
-    data = row.find_elements_by_tag_name('td')
-    file_row = []
-    for datum in data:
-        datum_text = datum.text
-        file_row.append(datum_text)
-    dist_data.append(','.join(file_row))
-
-#write distribution data into csv
-with open(save_location + '/dist_data.csv', 'wb') as f:
-	wr = csv.writer(f, dialect='excel')
-	wr.writerows(dist_data)
-
-#get defending data
-driver.get('https://members.fantasyfootballscout.co.uk/team-stats/defending/')
-table = driver.find_element_by_class_name('stats')
-body = table.find_element_by_tag_name('tbody')
-body_rows = body.find_elements_by_tag_name('tr')
-
-for row in body_rows:
-    data = row.find_elements_by_tag_name('td')
-    file_row = []
-    for datum in data:
-        datum_text = datum.text
-        file_row.append(datum_text)
-    def_data.append(','.join(file_row))
-
-#write defence data into csv
-with open(save_location + '/defence_data.csv', 'wb') as f:
-	wr = csv.writer(f, dialect='excel')
-	wr.writerows(def_data)
-
-#get attacking data
-driver.get('https://members.fantasyfootballscout.co.uk/team-stats/goal-threat/')
-table = driver.find_element_by_class_name('stats')
-body = table.find_element_by_tag_name('tbody')
-body_rows = body.find_elements_by_tag_name('tr')
-
-for row in body_rows:
-    data = row.find_elements_by_tag_name('td')
-    file_row = []
-    for datum in data:
-        datum_text = datum.text
-        file_row.append(datum_text)
-    attack_data.append(','.join(file_row))
-
-#write attacking data into csv
-with open(save_location + '/attacking_data.csv', 'wb') as f:
-	wr = csv.writer(f, dialect='excel')
-	wr.writerows(attack_data)
+for i in range(len(metrics)):
+	driver.get('https://members.fantasyfootballscout.co.uk/team-stats/%s/' % web_metrics[i])
+	table = driver.find_element_by_class_name('stats')
+	body = table.find_element_by_tag_name('tbody')
+	body_rows = body.find_elements_by_tag_name('tr')
 	
-#get set pieces data
-driver.get('https://members.fantasyfootballscout.co.uk/team-stats/set-pieces/')
-table = driver.find_element_by_class_name('stats')
-body = table.find_element_by_tag_name('tbody')
-body_rows = body.find_elements_by_tag_name('tr')
-
-for row in body_rows:
-    data = row.find_elements_by_tag_name('td')
-    file_row = []
-    for datum in data:
-        datum_text = datum.text
-        file_row.append(datum_text)
-    set_pieces_data.append(','.join(file_row))
-
-#write set pieces data into csv
-with open(save_location + '/set_pieces_data.csv', 'wb') as f:
-	wr = csv.writer(f, dialect='excel')
-	wr.writerows(set_pieces_data)
+	for row in body_rows:
+		data = row.find_elements_by_tag_name('td')
+		file_row = []
+		for datum_text = datum.text
+		file_row.append(datum_text)
+	metrics[i].append(','.join(file_row))
 	
-#get expected data
-driver.get('https://members.fantasyfootballscout.co.uk/team-stats/set-pieces/')
-table = driver.find_element_by_class_name('stats')
-body = table.find_element_by_tag_name('tbody')
-body_rows = body.find_elements_by_tag_name('tr')
-
-for row in body_rows:
-    data = row.find_elements_by_tag_name('td')
-    file_row = []
-    for datum in data:
-        datum_text = datum.text
-        file_row.append(datum_text)
-    ex_data.append(','.join(file_row))
-
-#write expected data into csv
-with open(save_location + '/expected_data.csv', 'wb') as f:
-	wr = csv.writer(f, dialect='excel')
-	wr.writerows(ex_data)
-
+	with open(save_location + '/%s.csv' % web_metrics[i], "w") as f:
+		f.write('/n'.join(metrics[i]))
