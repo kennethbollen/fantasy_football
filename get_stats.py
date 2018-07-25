a6aae64553de3202b5d4e5680aecc3558bcbfaac
kennethbollen <31512761+kennethbollen@users.noreply.github.com>
2018-07-25 14:35:49 +0100

    printing & var name fix
    added printing statement to help follow the flow of the code in action and fixed a wrongly worded variable from involv_data to invol_data on line 49

--- get_stats.py
+++ get_stats.py
@@ -10,26 +10,37 @@ password = 'Antwerp11'
 save_location = '/Users/2024450/Documents/Stats/fpl/csv_files'
 
 #store data
-invol_data = []
-dist_data = []
-def_data = []
-att_data = []
-set_pieces_data = []
-ex_data = []
-metrics = [involv_data, dist_data, def_data, att_data, set_pieces_data, ex_data]
+invol_data = [] #goal involvement
+dist_data = [] #distribution
+def_data = [] #defence
+att_data = [] #goal threat
+set_pieces_data = [] #set pieces
+ex_data = [] #expected
+metrics = [invol_data, dist_data, def_data, att_data, set_pieces_data, ex_data]
 web_metrics = ['involvement', 'distribution', 'defending', 'goal-threat', 'set-pieces', 'expected']
 	       
 #using Google Chrome browser, requires Chrome Driver to be installed into PATH
 driver = webdriver.Chrome('C:/Chrome_Driver/chromedriver.exe')
 
 #access fantasy football scout members
+print('accessing website...')
+print()
 driver.get('http://members.fantasyfootballscout.co.uk')
 
 #input user name and password
+print('inputting username...')
+print()
 driver.find_element(by='name', value='username').send_keys(username)
+print('inputting password...')
+print()
 driver.find_element(by='name', value='password').send_keys(password)
+print('logging in...')
+print()
 driver.find_element(by='name', value='login').click()
 
+#retrive team stats table and turn into csv
+print('retriving team data to be loaded as csv...')
+print()
 for i in range(len(metrics)):
 	driver.get('https://members.fantasyfootballscout.co.uk/team-stats/%s/' % web_metrics[i])
 	table = driver.find_element_by_class_name('stats')
